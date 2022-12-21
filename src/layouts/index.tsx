ef122fce43dd9d85a8aea91f55f5de1ee517dbc8
hyl <17803000829@163.com>
2022-12-22 01:02:48 +0800

    1

--- src/layouts/index.tsx
+++ src/layouts/index.tsx
@@ -1,12 +1,14 @@
 import { history, Outlet, useLocation } from "umi";
 import { Provider } from "react-redux";
-import { TabBar } from "antd-mobile";
+import { Button, TabBar } from "antd-mobile";
 import { useEffect, useState } from "react";
 import { store, persistor } from "@/store";
 import { PersistGate } from "redux-persist/integration/react";
 import AudioBar from "./AudioBar";
 import "animate.css";
 import "./index.less";
+import { request } from "@/utils";
+import cookie from "js-cookie";
 
 const tabs = [
   {
@@ -29,6 +31,15 @@ export default function Layout() {
 
   useEffect(() => {
     setActiveKey(location.pathname);
+
+    //种植会员token
+    if (!cookie.get("MUSIC_U")) {
+      cookie.set(
+        "MUSIC_U",
+        "bd1ea5d40e983b3d8028bcff22f35610f3850cd78108b09cc99953d869a4611c993166e004087dd370fc28c54e5e1bb43e9807d4b9f01ed20533926d55b6e50333aa127376130d47a0d2166338885bd7",
+        { expires: 30 }
+      );
+    }
   }, [location.pathname]);
 
   return (
@@ -46,6 +57,7 @@ export default function Layout() {
               <TabBar.Item key={item.key} title={item.title} />
             ))}
           </TabBar>
+
           <div className="main">
             <Outlet />
           </div>
